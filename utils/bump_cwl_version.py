1f7ca521e3a9cdb1fe41b78c2f59f98bec847f64
thomasyu888 <thomas.yu@sagebase.org>
2020-11-18 07:29:53 +0800

    Use mustache templating to increase the version

--- utils/bump_cwl_version.py
+++ utils/bump_cwl_version.py
@@ -5,6 +5,7 @@ import glob
 import logging
 import yaml
 
+import chevron
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -20,10 +21,10 @@ def tools_list(tools_dir):
   return glob.glob(glob_pattern)
 
 
-def read_tool(path):
-  with open(path) as file:
-    tool = yaml.load(file, Loader=yaml.FullLoader)
-  return tool
+# def read_tool(path):
+#   with open(path) as file:
+#     tool = yaml.load(file, Loader=yaml.FullLoader)
+#   return tool
 
 
 def parse_docker_image(tool):
@@ -70,7 +71,7 @@ def parse_docker_image(tool):
 
 
 def edit_tool(tool, new_version):
-  docker_image, parsing_metadata = parse_docker_image(tool)
+  # docker_image, parsing_metadata = parse_docker_image(tool)
   parts = docker_image.split(':')
   parts[-1] = new_version
   tool_obj = tool[parsing_metadata['docker_requirement_found_in']]
@@ -81,6 +82,16 @@ def edit_tool(tool, new_version):
     docker_requirement = next((item for item in tool_obj if item['class'] == 'DockerRequirement'))
   docker_requirement['dockerPull'] = new_docker_image
   return yaml.dump(tool)
+
+
+def create_tool(template_path, new_version, tools_dir):
+  cwl_input = {'version': new_version}
+  tool_name = os.path.basename(template_path).replace(".mustache", "")
+  with open(template_path, 'r') as mus_f:
+    template = chevron.render(mus_f, cwl_input)
+  tool_path = os.path.join(tools_dir, tool_name)
+  with open(tool_path, "w") as tool_f:
+      tool_f.write(template)
 
 
 def write_tool(path, output):
@@ -98,18 +109,25 @@ def parse_args():
   parser.add_argument(
     'new_version',
     help='New docker version to set in cwl tool')
+  parser.add_argument(
+    'template_dir',
+    help='Dir where CWL tool templates are stored')
   args = parser.parse_args()
-  return args.tool_dir, args.new_version
+  return args.tool_dir, args.new_version, args.template_dir
 
 
-def main(tools_dir, new_version):
-  tool_paths = tools_list(tools_dir)
-  for tool_path in tool_paths:
-    tool = read_tool(path=tool_path)
-    output = edit_tool(tool=tool, new_version=new_version)
-    write_tool(path=tool_path, output=output)
+def main(tools_dir, new_version, template_dir):
+  # tool_paths = tools_list(tools_dir)
+  template_paths = tools_list(template_dir)
+  for template_path in template_paths:
+    #tool = read_tool(path=tool_path)
+    create_tool(template_path=template_path, new_version=new_version,
+                tools_dir=tools_dir)
+    # output = edit_tool(tool=tool, new_version=new_version)
+    # write_tool(path=tool_path, output=output)
 
 
 if __name__ == '__main__':
-  tools_dir, new_version = parse_args()
-  main(tools_dir=tools_dir, new_version=new_version)
+  tools_dir, new_version, template_dir = parse_args()
+  main(tools_dir=tools_dir, new_version=new_version,
+       template_dir=template_dir)
