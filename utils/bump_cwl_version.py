80a4e4db1957731c9f5585447f13f660e0a843e8
thomasyu888 <thomas.yu@sagebase.org>
2020-11-18 08:04:50 +0800

    Remove stale code

--- utils/bump_cwl_version.py
+++ utils/bump_cwl_version.py
@@ -15,74 +15,12 @@ ERROR_UNEXPECTED_TYPE = 'Object is neither a list nor a dictionary'
 ERROR_MISSING_DOCKER_REQUIREMENT = 'CWL tool is missing DockerRequirement'
 ERROR_MISSING_DOCKER_PULL = 'Please specify "dockerPull" in your DockerRequirement and rerun script'
 
+
 def tools_list(tools_dir):
   log.debug(f'tools_dir passed to tools_list={tools_dir}')
   glob_pattern = f'{tools_dir}/*.mustache'
   log.debug(f'glob_pattern ={glob_pattern}')
   return glob.glob(glob_pattern)
-
-
-# def read_tool(path):
-#   with open(path) as file:
-#     tool = yaml.load(file, Loader=yaml.FullLoader)
-#   return tool
-
-
-def parse_docker_image(tool):
-
-  def find_docker_requirement(object):
-    if type(object) is dict:
-      docker_requirement = object.get('DockerRequirement', None)
-      object_type = dict
-    elif type(object) is list:
-      docker_requirement = next((item for item in object if item['class'] == 'DockerRequirement'), None)
-      object_type = list
-    else:
-      raise ValueError(ERROR_UNEXPECTED_TYPE)
-
-    return docker_requirement,object_type
-
-  docker_requirement = None
-
-  if 'hints' in tool:
-    docker_requirement, object_type = find_docker_requirement(tool['hints'])
-    if docker_requirement is not None:
-      parsing_metadata = {
-        'docker_requirement_found_in': 'hints',
-        'object_type': object_type
-      }
-
-  if docker_requirement is None and 'requirements' in tool:
-    docker_requirement, object_type = find_docker_requirement(tool['requirements'])
-    if docker_requirement is not None:
-      parsing_metadata = {
-        'docker_requirement_found_in': 'requirements',
-        'object_type': object_type
-      }
-
-  if docker_requirement is None:
-    raise ValueError(ERROR_MISSING_DOCKER_REQUIREMENT)
-
-  docker_image = docker_requirement.get('dockerPull', None)
-
-  if docker_image is None:
-    raise ValueError(ERROR_MISSING_DOCKER_PULL)
-
-  return docker_image, parsing_metadata
-
-
-def edit_tool(tool, new_version):
-  # docker_image, parsing_metadata = parse_docker_image(tool)
-  parts = docker_image.split(':')
-  parts[-1] = new_version
-  tool_obj = tool[parsing_metadata['docker_requirement_found_in']]
-  new_docker_image = ':'.join(parts)
-  if parsing_metadata['object_type'] is dict:
-    docker_requirement = tool_obj['DockerRequirement']
-  else:
-    docker_requirement = next((item for item in tool_obj if item['class'] == 'DockerRequirement'))
-  docker_requirement['dockerPull'] = new_docker_image
-  return yaml.dump(tool)
 
 
 def create_tool(template_path, new_version, tools_dir):
@@ -118,14 +56,10 @@ def parse_args():
 
 
 def main(tools_dir, new_version, template_dir):
-  # tool_paths = tools_list(tools_dir)
   template_paths = tools_list(template_dir)
   for template_path in template_paths:
-    #tool = read_tool(path=tool_path)
     create_tool(template_path=template_path, new_version=new_version,
                 tools_dir=tools_dir)
-    # output = edit_tool(tool=tool, new_version=new_version)
-    # write_tool(path=tool_path, output=output)
 
 
 if __name__ == '__main__':
